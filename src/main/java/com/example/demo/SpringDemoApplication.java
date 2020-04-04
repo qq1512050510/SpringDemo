affc6d8e497176df9798c7068e601cec25d60e9e
jiangyupeng <jiangyupeng@bjsasc.com>
2020-04-05 07:10:51 +0800

    DataSourceAutoConfiguration DataSourceAutoConfiguration未配置

--- src/main/java/com/example/demo/SpringDemoApplication.java
+++ src/main/java/com/example/demo/SpringDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//未配置数据源启动 (exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {

9aff6bbae42549186f145e235764b32e8b1f7743
qq1512050510 <jyp@192.168.0.6>
2020-04-17 14:43:21 +0800

    Spring Profiles多环境配置

--- src/test/java/com/example/demo/profile/SpringProfileTest.java
+++ src/test/java/com/example/demo/profile/SpringProfileTest.java
@@ -1,17 +1,22 @@
 package com.example.demo.profile;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.demo.SpringDemoApplication;
 import com.example.demo.bean.Person;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+//Spring整合JUnit4测试时，使用注解引入多个配置文件
+//@ContextConfiguration(Locations="../application.yml")
+//@ContextConfiguration(locations = { "classpath*:/application.yml", "classpath*:/spring2.xml" }) 
 //@ContextConfiguration(classes = SpringDemoApplication.class)
 @SpringBootTest(classes=SpringDemoApplication.class)
-@ActiveProfiles("dev")
+@ActiveProfiles("prod")
 public class SpringProfileTest {
 	@Autowired
 	Person p;
@@ -19,7 +24,7 @@ public class SpringProfileTest {
 	@Test
 	public void testProfile() {
 		System.out.println("test start");
-		//p.speak();
+		p.speak();
 	}
 
 }
